b8c076989307095f846d71bc5b0c6caae6bc98f4
Jordan Horwich <jordan@voidingwarranties.com>
2014-03-30 04:21:52 -0400

    Implement video feed mode and image directory mode.

--- src/SignSight.cpp
+++ src/SignSight.cpp
@@ -1,13 +1,20 @@
 #include <unistd.h>
 #include <cstdlib>
+#include <dirent.h>
 
+#include <opencv2/highgui/highgui.hpp>
+
+#include <list>
 #include <iostream>
+
+
+int displayOutput(bool video_flag, int video_index, bool dir_flag, char* dir_path);
 
 void print_usage(char* name)
 {
-    std::cout << "Usage: " << name << " [-v <video_device_index> | -f <image_directory_path>]"
+    std::cerr << "Usage: " << name << " [-v <video_device_index> | -f <image_directory_path>]"
               << "\n\n"
-              << "       if no options are specified video device 0 is used by default."
+              << "       If no options are specified video device 0 is used by default."
               << std::endl;
 }
 
@@ -43,6 +50,84 @@ int main(int argc, char** argv)
         if (video_flag && dir_flag) {
             print_usage(argv[0]);
             return 1;
+        }
+    }
+
+    int return_val = displayOutput(video_flag, video_index, dir_flag, dir_path);
+
+    return return_val;
+}
+
+int displayOutput(bool video_flag, int video_index, bool dir_flag, char* dir_path)
+{
+    // The right and left arrow key codes seem to differ from system to system.
+    // Change these values as necessary
+    const int RIGHT_ARROW_KEY = 65363;
+    const int LEFT_ARROW_KEY = 65361;
+
+
+    cv::namedWindow("Output");
+    cv::Mat image;
+
+    if (video_flag) {
+        cv::VideoCapture cam(video_index);
+
+        while (true) {
+            cam >> image;
+            if (! image.data) {
+                std::cerr << "Error reading camera data. Did someone unplug it?" << std::endl;
+            }
+            cv::imshow("Output", image);
+
+            int key = cv::waitKey(10);
+            if (key == 'q') {
+                break;
+            }
+        }
+    } else if (dir_flag) {
+        std::list<char*> file_list;
+        DIR* dir = opendir(dir_path);
+        if (! dir) {
+            std::cerr << "Directory " << dir_path << " does not exist!" << std::endl;
+            return 1;
+        }
+        // Iterate over all files in the specified directory.
+        dirent* file;
+        while ((file = readdir(dir)) != NULL) {
+            if (file && file->d_type == DT_REG) {
+                // Form the path by concatenating dir_path with each file name
+                char* buffer = new char[strlen(dir_path) + strlen(file->d_name) + 2]();
+                strcpy(buffer, dir_path);
+                buffer[strlen(dir_path)] = '/';
+                strcpy(buffer + strlen(dir_path) + 1, file->d_name);
+                // add the buffer to the end of the doubly linked list
+                file_list.push_back(buffer);
+            }
+        }
+        std::list<char*>::const_iterator file_itr = file_list.begin();
+        while (true) {
+            image = cv::imread(*file_itr);
+            if (! image.data) {
+                std::cerr << "Error reading image file " << *file_itr << std::endl;
+                return 1;
+            }
+
+            cv::imshow("Output", image);
+
+            int key = cv::waitKey();
+            if (key == 'q') {
+                break;
+            } else if (key == RIGHT_ARROW_KEY) {
+                ++file_itr;
+                if (file_itr == file_list.end()) {
+                    file_itr = file_list.begin();
+                }
+            } else if (key == LEFT_ARROW_KEY) {
+                --file_itr;
+                if (file_itr == file_list.end()) {
+                    --file_itr;
+                }
+            }
         }
     }
 
