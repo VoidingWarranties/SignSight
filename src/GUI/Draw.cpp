10adf0c2a13a32d11c2b53b669b7852218a486f6
Jordan Horwich <jordan@voidingwarranties.com>
2014-04-03 00:00:01 -0400

    Add rectangle scaling function

--- src/GUI/Draw.cpp
+++ src/GUI/Draw.cpp
@@ -7,15 +7,31 @@
 
 void drawBoundingRectangles(cv::Mat& image, const std::vector<std::vector<cv::Point> >& contours, const cv::Scalar& color, const float scale)
 {
-    const float position_scale = (scale - 1) / 2;
     for (size_t i = 0; i < contours.size(); ++i) {
         // Scale the rectangle.
-        cv::Rect bounding_rect = cv::boundingRect(contours[i]);
-        bounding_rect.x -= position_scale * bounding_rect.width;
-        bounding_rect.y -= position_scale * bounding_rect.height;;
-        bounding_rect.width *= scale ;
-        bounding_rect.height *= scale ;
+        cv::Rect bounding_rect = scaleRectWithLimits(cv::boundingRect(contours[i]), cv::Point(0,0), cv::Point(image.cols,image.rows), scale);
         // Draw the rectangle.
         cv::rectangle(image, bounding_rect.tl(), bounding_rect.br(), color, 2);
     }
+}
+
+cv::Rect scaleRectWithLimits(const cv::Rect& source_rect, const cv::Point& tl_limit, const cv::Point& br_limit, float scale)
+{
+    float position_scale = (scale - 1) / 2;
+
+    int start_x = source_rect.x - (position_scale * source_rect.width);
+    int start_y = source_rect.y - (position_scale * source_rect.height);
+    int end_x = start_x + (scale * source_rect.width);
+    int end_y = start_y + (scale * source_rect.height);
+
+    if (start_x < tl_limit.x)
+        start_x = tl_limit.x;
+    if (start_y < tl_limit.y)
+        start_y = tl_limit.y;
+    if (end_x > br_limit.x)
+        end_x = br_limit.x;
+    if (end_y > br_limit.y)
+        end_y = br_limit.y;
+
+    return cv::Rect(start_x, start_y, (end_x - start_x), (end_y - start_y));
 }
